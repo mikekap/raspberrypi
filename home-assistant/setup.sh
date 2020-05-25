ddc46a6e63059514f92d310ebb503867750c70cb
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-05-24 19:52:16 -0700

    lol

--- home-assistant/setup.sh
+++ home-assistant/setup.sh
@@ -2,7 +2,7 @@
 set -meo pipefail
 
 sudo mkdir -p /opt/home-assistant /etc/docker-compose/home-assistant
-sudo cp ./unifi/docker-compose.yml /etc/docker-compose/home-assistant
+sudo cp ./home-assistant/docker-compose.yml /etc/docker-compose/home-assistant
 sudo systemctl daemon-reload
 sudo systemctl start docker-compose@home-assistant
 sudo systemctl enable docker-compose@home-assistant
