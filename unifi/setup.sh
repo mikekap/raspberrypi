47b15938f3e0aba51dbe15af8c989952fe215ce3
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-05-24 19:11:08 -0700

    More fixes

--- unifi/setup.sh
+++ unifi/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -meo pipefail
 
-sudo mkdir -p /opt/unifi /opt/mongodb /etc/docker-compose/unifi/
+sudo mkdir -p /opt/unifi/data /opt/mongodb /etc/docker-compose/unifi/
 sudo cp ./unifi/docker-compose.yml /etc/docker-compose/unifi/
 sudo systemctl daemon-reload
 sudo systemctl start docker-compose@unifi
