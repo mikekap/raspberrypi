407a0c4d1c207eaa70bdb6c8e4986ed5ba37f5fd
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-06-01 23:50:46 -0700

    Add mosquitto broker

---
+++ mosquitto/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -meo pipefail
+
+sudo mkdir -p /opt/mosquitto/config /opt/mosquitto/data /etc/docker-compose/home-assistant
+sudo cp ./mosquitto/mosquitto.conf /opt/mosquitto/config/
+sudo cp ./mosquitto/docker-compose.yml /etc/docker-compose/mosquitto
+sudo systemctl daemon-reload
+sudo systemctl start docker-compose@mosquitto
+sudo systemctl enable docker-compose@mosquitto
+
+
