76e4ce270492ee18ba9ef4e0222dc4682a0fb5d7
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-06-02 00:22:16 -0700

    more setup

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-.PHONY: base unifi
+.PHONY: base unifi light-control
+all: base unifi light-control
 
 base:
 	cd base && docker build -t rpi-base .
 
 unifi: base
 	cd unifi && docker build -t rpi-unifi .
+
+light-control:
+	cd light-control && docker built -t rpi-light-control
---
+++ light-control/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -meo pipefail
+
+sudo mkdir -p /etc/docker-compose/light-control/
+sudo cp ./light-control/docker-compose.yml /etc/docker-compose/light-control/
+sudo systemctl daemon-reload
+sudo systemctl start docker-compose@light-control
+sudo systemctl enable docker-compose@light-control
+
