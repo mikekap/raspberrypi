b106c0263b8fadfa68581ff1fbf349c182572189
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-06-01 23:53:17 -0700

    fix

--- mosquitto/setup.sh
+++ mosquitto/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -meo pipefail
 
-sudo mkdir -p /opt/mosquitto/config /opt/mosquitto/data /etc/docker-compose/home-assistant
+sudo mkdir -p /opt/mosquitto/config /opt/mosquitto/data /etc/docker-compose/mosquitto/
 sudo cp ./mosquitto/mosquitto.conf /opt/mosquitto/config/
-sudo cp ./mosquitto/docker-compose.yml /etc/docker-compose/mosquitto
+sudo cp ./mosquitto/docker-compose.yml /etc/docker-compose/mosquitto/
 sudo systemctl daemon-reload
 sudo systemctl start docker-compose@mosquitto
 sudo systemctl enable docker-compose@mosquitto
