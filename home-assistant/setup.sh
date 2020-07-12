aada05d4fc2abc5226c24a5109d6f80ed67e882d
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-07-12 16:13:26 -0700

    copy the config

--- home-assistant/setup.sh
+++ home-assistant/setup.sh
@@ -3,8 +3,7 @@ set -meo pipefail
 
 sudo mkdir -p /opt/home-assistant /etc/docker-compose/home-assistant
 sudo cp ./home-assistant/docker-compose.yml /etc/docker-compose/home-assistant
+sudo cp ./home-assistant/configuration.yaml /opt/home-assistant/configuration.yaml
 sudo systemctl daemon-reload
 sudo systemctl start docker-compose@home-assistant
 sudo systemctl enable docker-compose@home-assistant
-
-
