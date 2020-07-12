5cfd459180c0b2f57fdb0a5f81c8bdc57a69fb0a
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-07-12 16:10:43 -0700

    Add tv config

--- light-control/server.py
+++ light-control/server.py
@@ -6,15 +6,21 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     client.subscribe("home/living/light/#")
+    client.subscribe("home/living/tv/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    if msg.topic.endswith('/toggle'):
-        print('power',str(msg.payload))
-        subprocess.check_call("ir-ctl -S nec:0x404", shell=True)
-    if msg.topic.endswith('/toggle-night'):
-        print('power-night',str(msg.payload))
-        subprocess.check_call("ir-ctl -S nec:0x405", shell=True)
+    if msg.topic.startswith('home/living/light'):
+        if msg.topic.endswith('/toggle'):
+            print('power-light', str(msg.payload))
+            subprocess.check_call("ir-ctl -S nec:0x404", shell=True)
+        if msg.topic.endswith('/toggle-night'):
+            print('power-night', str(msg.payload))
+            subprocess.check_call("ir-ctl -S nec:0x405", shell=True)
+    elif msg.topic.startswith('home/living/tv'):
+        if msg.topic.endswith('/toggle'):
+            print('power-tv', str(msg.payload))
+            subprocess.check_call('ir-ctl -S necx:0x70702', shell=True)
 
     print(msg.topic+" "+str(msg.payload))
 
